36223e9f1b263129290b61886d8f3f9059a507c8
Steven Harlow <stharlow@gmail.com>
2014-11-12 15:34:50 -0800

    some vote validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,13 @@
 class Vote < ActiveRecord::Base
+  validates :survey_id, :uniqueness => {:scope => :user_id, :message => "can only be voted on once" }
+  validate :survey_not_expired
+
   belongs_to :user
   belongs_to :survey
+
+  def survey_not_expired
+    if self.survey.ended?
+      errors.add(:survey, "has ended")
+    end
+  end
 end
