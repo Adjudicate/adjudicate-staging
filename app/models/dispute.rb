c566a457668f0172aadcc2e77d133f7465cf021b
Steven Harlow <stharlow@gmail.com>
2014-11-18 10:03:47 -0800

    destroy documents when destroying dispute

--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -1,5 +1,5 @@
 class Dispute < ActiveRecord::Base
-  has_many :dispute_documents
+  has_many :dispute_documents, dependent: :destroy
   has_one :survey
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
 
