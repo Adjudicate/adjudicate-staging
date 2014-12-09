f29cc56d18e4628c1c96f6816a4c4d53c586bfb3
Steven Harlow <stharlow@gmail.com>
2014-12-09 01:30:53 -0800

    forgotten password link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:username, :email)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
