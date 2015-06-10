ec5c118270b4b025e84a222ddc41e24cd350ffd1
Ben Calegari <bencalegari@gmail.com>
2015-06-10 15:34:22 -0400

    Redirect back to disputes page after vote is changed (edited)

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -17,10 +17,11 @@ class VotesController < ApplicationController
     dispute = Dispute.find(params[:dispute_id])
     @vote = Vote.where(user_id: current_user.id, survey_id: dispute.survey.id).first
     if @vote.update_attributes(vote_params)
-      flash[:notice] = 'Thank you for voting'
-      redirect_to dispute_vote_path(dispute, uid: dispute.uid)
+      flash[:notice] = "Thank you for voting on Dispute ##{dispute.id}"
+      redirect_to disputes_path
     else
-
+      flash[:notice] = 'Vote was not processed, try again.'
+      redirect_to disputes_path
     end
   end
 
