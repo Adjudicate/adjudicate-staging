0428d03d6c24c260f1555d37f9c64d5ef699eeb7
Nick Borbe <developer@adjudicateonline.com>
2016-04-17 13:25:35 -0400

    removed stripe initializer from gitignore and removed variables from hard code

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,9 @@
+Rails.configuration.stripe = {
+  :publishable_key => ENV['STRIPE_PUBLIC_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+
+
